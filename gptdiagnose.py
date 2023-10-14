0329a79cc423bb212d8ca1b147936b4a8d40e447
Edward Wang <115112114+Edward-W7@users.noreply.github.com>
2023-10-14 16:31:45 -0400

    Update gptdiagnose.py

--- gptdiagnose.py
+++ gptdiagnose.py
@@ -3,7 +3,8 @@ openai.api_key = 'PUT API KEY HERE'
 messages = [{"role": "system", "content": "You are an intelligent assistant designed to diagnose disorders based on symptoms. "
             "A user will prompt you with a description of their symptoms, and you will provide 2-3 possible diseases, illnesses, or disorders they could have."
                                           "For each possible disorder, offer some advice or treatment tips based on the disorder"
-                                          "After diagnosing them, the user may want to ask more questions about their disorder, or clarify more information."}]
+                                          "After diagnosing them, the user may want to ask more questions about their disorder, or clarify more information."
+                                          "The user already knows that you are an AI assistant and that information you provide may not be accurate, there is no need to reclarify."}]
 while True: 
     message = input("User : ") 
     if message: 
