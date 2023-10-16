47dc877fbe5f1ed8550f4868dede6b5b83964f15
clairewyh <147781583+clairewyh@users.noreply.github.com>
2023-10-15 20:58:29 -0400

    Create contact.jsx

---
+++ contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="custom-background">
+      <h2>Meet the Rockers team!</h2>
+      <img src="chloe.jpg" width="197px" height="262px" hspace="8px"></img>
+      <img src="linkss.png" width="197px" height="262px" hspace="8px"></img>
+      <img src="claire.jpg" width="197px" height="262px" hspace="8px"></img>
+      <img src="ishika.png" width="197px" height="262px" hspace="8px" ></img>
+      <br></br>
+      <a href="mailto:chloe.c.trinh@gmail.com" hspace="8px">Chloe Trinh </a>
+      <a href="edward.wang308@gmail.com" hspace="8px">Edward Wang </a>
+      <a href="claire.wyh2@gmail.com" hspace="8px">Claire Wang </a>
+      <a href="ishikarath2016@gmail.com" hspace="8px">Ishika Rath </a>
+      
+    </div>
+  );
+}
+
+export default Contact;
