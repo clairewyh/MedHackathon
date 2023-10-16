36d9d640eaf48408a5dbff060746061a7d6f3f67
clairewyh <147781583+clairewyh@users.noreply.github.com>
2023-10-15 22:14:22 -0400

    Create about.jsx

---
+++ about.jsx
@@ -0,0 +1,14 @@
+// About.js
+
+import React from 'react';
+
+function About() {
+  return (
+    <div className="custom-background">
+      <h2>Our Goal</h2>
+      <p>The "Bad Blood?" project was created by the Rockers; a group of 4 grade 11's at John Fraser Secondary School. The goal of Bad Blood is to help easily diagnose people from the comfort of their own home. To get a diagnosis all the user has to do is enter their symptoms.</p>
+    </div>
+  );
+}
+
+export default About;
