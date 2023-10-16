e85d1df52693fae61e20517b8c35aa1c659fc743
clairewyh <147781583+clairewyh@users.noreply.github.com>
2023-10-15 20:58:01 -0400

    Create resource.jsx

---
+++ resource.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Resources() {
+  return (
+    <div className="custom-background">
+      <h2>Resources</h2>
+      <p>Resources will be added later!</p>
+    </div>
+  );
+}
+
+export default Resources;
